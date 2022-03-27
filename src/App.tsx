e75cc16e3272683f381d3e54e5b25e4ad473bc60
John Chen <chenjohn.8p@gmail.com>
2022-03-27 16:35:44 -0500

    basic initial plane in starry background

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Backdrop, Html, OrbitControls, PerspectiveCamera, Stars } from "@react-three/drei";
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from "react-three-fiber";
+import { DoubleSide } from "three";
 
-function App() {
+import "./style.css";
+
+function GreenSquare({position, args}: {position:[number,number,number], args:[number,number]}) {
+  const ref = useRef<THREE.Mesh>(null!)
+  useFrame(() => (ref.current.rotation.z += 0.001))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh 
+      ref = {ref}
+      position = {position}
+      rotation = {[Math.PI / 2, 0, 0]}
+      scale = {[1, 1, 1]}
+    >
+    <planeBufferGeometry args={args}/>
+    <meshBasicMaterial color="orange" side={DoubleSide} />
+    </mesh>
+  )
 }
 
-export default App;
+function ToolTip() {
+  return (
+    <Html center position = {[0, 0.5, 0]}>
+      <p>Hakaton!</p>
+    </Html>
+  )
+}
+
+export default function App() {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <color attach="background" args={['black']} />
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <PerspectiveCamera position={[10, 10, 10]} makeDefault />
+      <GreenSquare position={[0, 0, 0]} args={[10, 10]}/>
+      <ToolTip />
+
+    </Canvas>
+    
+  )
+}
+
